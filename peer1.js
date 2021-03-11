48ac39d8d5e4062f66404dad9c47891d34715cd9
pj8912 <jpintofun2b@gmail.com>
2021-03-11 12:22:34 +0530

    Main commit

---
+++ peer1.js
@@ -0,0 +1,34 @@
+
+const localconnection = new RTCPeerConnection();
+
+// creating datachannel
+const dataChannel= localconnection.createDataChannel("channel");
+
+dataChannel.onmessage = e => console.log("Message from remote: "+ e.data)
+
+dataChannel.onopen = e => console.log("Connection Opend");
+
+// ice candidate for localconnection which gives SDP
+localconnection.onicecandidate = e => console.log("new candidate: "+ JSON.stringify(localconnection.localDescription));
+
+// creating offer to connect with another peer
+localconnection.createOffer()
+.then(x => localconnection.setLocalDescription(x))
+.then(console.lo("created"));
+
+// this generates as "new candidate" string.
+// this is peer1's localDescription
+// copy the json format(" {} ") inside the string and place it as 'offer' in the new browser
+// this is the offer to connect to peer2
+
+// --------------------------------------------------------------------------
+
+// creating 'answer' from another browser
+// which is peer2's localDescription
+// this is the 'answer' of peer2 to the 'offer' of peer1.
+
+const answer = {} ;
+
+localconnection.setRemoteDescription(answer);
+
+
---
+++ peer2.js
@@ -0,0 +1,37 @@
+// !!new browser
+
+// taking the localDescription of peer1 we create the 'offer'
+
+
+const offer = {}; //{....}
+
+//peer2 as remote
+const remoteconnection = new RTCPeerConnection();
+
+// peer2 ice candidate
+remoteconnection.onicecandidate = e => console.log("ice candidate: " + JSON.stringify(remoteconnection.localDescription));
+
+
+remoteconnection.ondatachannel = e => {
+
+    const receiverChannel = e.channel;
+    receiverChannel.onmessage = e => console.log("message from local: " + e.data);
+    receiverChannel.onopen = e => console.log("opend!");
+    // closing....
+    receiverChannel.oncolse = e => alert('closed')
+}
+
+
+
+// peer2's remoteDescription which intakes 'offer'
+remoteconnection.setRemoteDescription(offer)
+    .then(x => console.log("done"))
+
+// creating answer for 'offer'
+remoteconnection.createAnswer()
+    .then(x => remoteconnection.setLocalDescription(x))
+    .the(x => console.log('success'))
+
+// ........................
+// back to peer1.js(1'st browswer)
+
